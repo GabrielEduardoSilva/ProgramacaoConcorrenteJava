9661494783573dfb07cb00d2b169267f2580d82e
GabrielEduardoSilva <102189527+GabrielEduardoSilva@users.noreply.github.com>
2024-09-19 23:38:09 -0300

    Definindo atributos da TemperaturaDiaria

--- ExperimentoTemperaturas/src/TemperaturaDiaria.java
+++ ExperimentoTemperaturas/src/TemperaturaDiaria.java
@@ -1,2 +1,8 @@
 public class TemperaturaDiaria {
+    private String pais;
+    private String cidade;
+    private int mes;
+    private int dia;
+    private int ano;
+    private double temperatura;
 }
